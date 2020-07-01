ba2a0b98ecfc7893926dfd150ff171ae7266efcd
Carlos Ivis <52825816+carlosivis@users.noreply.github.com>
2020-07-01 11:59:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+class NyanCat{
+    constructor(){
+        const nyan = ['/images/nyanCat.gif','/images/nyanCatGold.gif',
+            '/images/nyanCatMexico.gif','/images/nyanCatPikachu.gif']
+        this.x = -800
+        this.y = 0
+        this.baseY = Math.random()*window.innerWidth
+        this.speed = Math.random()*50
+        this.image = document.createElement('img')
+        this.image.src = nyan[Math.floor(Math.random()*4)]
+        this.image.alt = 'Nyan Cat Random'
+        document.querySelector('#cats').appendChild(this.image)
+    }
+    move(){
+        this.x += this.speed
+        this.y = this.baseY + Math.cos(this.x/.3)*20
+        if(this.x- this.x/2 >= window.innerWidth){
+            this.x = -800
+        }
+        this.image.style.transform = `translate(${this.x}px, ${this.y}px)`
+    }
+
+}
+
+function add(){
+    let newEl = document.querySelector('#addCat')
+        newEl.addEventListener('click', () => {
+            cats.push(new NyanCat())
+        });
+}
